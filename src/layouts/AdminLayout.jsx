e868da0d354d544836e1e1a989ba9a61e2f31607
f0230 <fran.salisg@gmail.com>
2025-06-12 20:06:07 -0300

    asd

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
+
 import AdminSidebarDesktop from '../components/AdminSidebarDesktop';
 import AdminSidebarMobile from '../components/AdminSidebarMobile';
 
 const AdminLayout = ({ children }) => {
+
+    useEffect(() => {
+        const setViewportHeight = () => {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        };
+        setViewportHeight();
+        window.addEventListener('resize', setViewportHeight);
+        return () => window.removeEventListener('resize', setViewportHeight);
+    }, []);
+    
+
+    
+    
     return (
         <div className="relative flex min-h-screen text-white">
 
