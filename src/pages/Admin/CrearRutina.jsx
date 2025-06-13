448cabe1912be82527d9d2ae0cf20ce209636c9e
f0230 <fran.salisg@gmail.com>
2025-06-13 02:15:33 -0300

    asd

--- src/pages/Admin/CrearRutina.jsx
+++ src/pages/Admin/CrearRutina.jsx
@@ -1,20 +1,18 @@
-
 import RutinaForm from '../../components/Rutina/RutinaForm';
 import AdminLayout from '../../layouts/AdminLayout';
-// src/pages/Admin/CrearRutina.jsx
 import React from 'react';
 
 const CrearRutina = () => {
     return (
-        <AdminLayout> 
-        <div className="min-h-screen px-4 py-8 md:px-10 text-white">
-            <div className="max-w-5xl mx-auto space-y-6">
-                <div className="flex items-center justify-between">
-                    <h1 className="text-3xl font-bold">🧱 Crear nueva rutina</h1>
+        <AdminLayout>
+            <div className="h-full w-full px-4 md:px-10 text-white overflow-hidden">
+                <div className="max-w-5xl mx-auto space-y-6 py-6">
+                    <div className="flex items-center justify-between">
+                        <h1 className="text-3xl font-bold">🧱 Crear nueva rutina</h1>
+                    </div>
+                    <RutinaForm />
                 </div>
-                <RutinaForm />
             </div>
-        </div>
         </AdminLayout>
     );
 };
