fa7d350b0face547ded965253ddb67eed6ba58ba
f0230 <fran.salisg@gmail.com>
2025-07-21 20:09:31 -0300

    Asd

--- src/pages/Admin/AdminRutinas.jsx
+++ src/pages/Admin/AdminRutinas.jsx
@@ -3,9 +3,7 @@ import RutinasManager from '../../components/RutinasManager';
 
 const AdminRutinas = () => {
     return (
-            <div className="space-y-10 px-4 md:px-8 py-10">
-                <RutinasManager />
-            </div>
+        <RutinasManager />
     );
 };
 
