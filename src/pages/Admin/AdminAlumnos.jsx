00056c47b57f54a424c79700402f9d6bfc4614a8
f0230 <fran.salisg@gmail.com>
2025-07-21 20:44:49 -0300

    asd

--- src/pages/Admin/AdminAlumnos.jsx
+++ src/pages/Admin/AdminAlumnos.jsx
@@ -4,7 +4,7 @@ import AlumnosManager from '../../components/AlumnosManager';
 const AdminAlumnos = () => {
     return (
         // Ya no se envuelve con <AdminLayout>
-        <div className="p-6 max-w-6xl mx-auto space-y-6 text-white pb-[calc(4rem+env(safe-area-inset-bottom))]">
+        <div className="p-6 max-w-6xl mx-auto space-y-6 text-white">
             <AlumnosManager />
         </div>
     );
