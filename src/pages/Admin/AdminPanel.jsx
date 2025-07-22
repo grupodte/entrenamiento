7d2b1aa7795af45c827c4e401706fe627cd28132
f0230 <fran.salisg@gmail.com>
2025-07-21 22:03:05 -0300

    as

--- src/pages/Admin/AdminPanel.jsx
+++ src/pages/Admin/AdminPanel.jsx
@@ -1,9 +1,12 @@
 const AdminPanel = () => (
-  <div className="max-w-3xl mx-auto mt-10 px-6 py-8 rounded-xl bg-white/20 backdrop-blur-md shadow-md border border-white/10">
-    <h1 className="text-2xl font-bold text-white mb-4">Bienvenido al panel del entrenador</h1>
-    <p className="text-gray-200">
-      Usá el menú lateral para gestionar <strong>alumnos</strong>, <strong>rutinas</strong> o <strong>ejercicios</strong>.
-    </p>
+  <div className=" inset-0 w-full h-full  bg-cover bg-center mt-[25px] items-center">
+    <div className=" inset-0 backdrop-blur-sm " />
+
+    <div className="relative z-10 max-w-[300px] mx-auto px-6 py-8 rounded-xl bg-white/5 backdrop-blur-md shadow-md border border-white/10 text-white">
+      <h1 className="text-2xl font-bold mb-4">Bienvenido al Dashboard del entrenador</h1>
+      <p className="text-gray-200">  por ahora solo podes crear ejercicio, rutinas y asignarlas a alumnos
+      </p>
+    </div>
   </div>
 );
 
