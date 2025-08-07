cc796a97e1d73c81b2e82f0446a3ae51150779de
f0230 <f02301111@gmail.com>
2025-08-07 01:13:37 -0300

    asd

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
     return (
-        <div className="main-content scroll-smooth-hidden"
+        <div className="main-content content-wrapper  scroll-smooth-hidden"
 >
        
                 <Outlet />
